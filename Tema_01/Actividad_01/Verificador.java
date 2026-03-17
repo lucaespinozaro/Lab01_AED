5f939579d4a3491d20c973447b4b198e51a99f8d
saluhera2023 <saluhera2023@gmail.com>
2026-03-17 06:17:57 -0500

    Update Verificador.java

--- Tema_01/Actividad_01/Verificador.java
+++ Tema_01/Actividad_01/Verificador.java
@@ -36,9 +36,9 @@ public class Verificador {
         double r2_minY = r2.getEsquina1().getY();
         double r2_maxY = r2.getEsquina2().getY();
 
-        double maxMinX = (r1_minX > r2_minX) ? r1_minX : r2_minX;
-        double minMaxX = (r1_maxX < r2_maxX) ? r1_maxX : r2_maxX;
-        boolean comparteX = maxMinX < minMaxX;
+        double mayMinX = (r1_minX > r2_minX) ? r1_minX : r2_minX;
+        double menMaxX = (r1_maxX < r2_maxX) ? r1_maxX : r2_maxX;
+        boolean comparteX = mayMinX < menMaxX;
 
         double maxMinY = (r1_minY > r2_minY) ? r1_minY : r2_minY;
         double minMaxY = (r1_maxY < r2_maxY) ? r1_maxY : r2_maxY;
