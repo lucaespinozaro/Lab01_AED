ccb02dda85e94ee4b43f5b258fed966199b611f2
saluhera2023 <saluhera2023@gmail.com>
2026-03-16 20:17:53 -0500

    Update Main.java

--- Tema_01/Actividad_01/Main.java
+++ Tema_01/Actividad_01/Main.java
@@ -1,35 +1,42 @@
+package Actividad_01;
+
 import java.util.Scanner;
 
-public class Main{
-  public static void main(){
+public class Main {
 
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Ingrese una esquina del 1er rectángulo: ");
-    double x1 = sc.nextDouble();
-    double y1 = sc.nextDouble();
-    Coordenada c1 = new Coordenada(x1, y1);
+    public static void main(String[] args) {
 
-    System.out.print("Ingrese otra esquina del 1er rectángulo: ");
-    double x2 = sc.nextDouble();
-    double y2 = sc.nextDouble();
-    Coordenada c2 = new Coordenada(x2, y2);
+        Scanner sc = new Scanner(System.in);
 
-    Rectangulo r1 = new Rectangulo(c1, c2);
+        System.out.println("Rectangulo A");
+        System.out.print("Ingrese una esquina del 1er rectangulo: ");
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+        Coordenada a1 = new Coordenada(x1, y1);
 
-    System.out.print("Ingrese una esquina del 2do rectángulo: ");
-    x1 = sc.nextDouble();
-    y1 = sc.nextDouble();
-    c1 = new Coordenada(x1, y1);
+        System.out.print("Ingrese la esquina opuesta del 1er rectangulo: ");
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+        Coordenada a2 = new Coordenada(x2, y2);
 
-    System.out.print("Ingrese otra esquina del 2do rectángulo: ");
-    x2 = sc.nextDouble();
-    y2 = sc.nextDouble();
-    c2 = new Coordenada(x2, y2);
+        Rectangulo A = new Rectangulo(a1, a2);
 
-    Rectangulo r2 = new Rectangulo(c1, c2);
+        System.out.println("Rectangulo B");
+        System.out.print("Ingrese una esquina del 2do rectangulo: ");
+        x1 = sc.nextDouble();
+        y1 = sc.nextDouble();
+        Coordenada b1 = new Coordenada(x1, y1);
 
-    System.out.print("Ingrese una opcion \n1. Sobrepuesto ");
+        System.out.print("Ingrese la esquina opuesta del 2do rectangulo: ");
+        x2 = sc.nextDouble();
+        y2 = sc.nextDouble();
+        Coordenada b2 = new Coordenada(x2, y2);
 
-    
-  }
+        Rectangulo B = new Rectangulo(b1, b2);
+
+        System.out.println("Rectángulo A = " + A);
+        System.out.println("Rectángulo B = " + B);
+
+        sc.close();
+    }
 }
