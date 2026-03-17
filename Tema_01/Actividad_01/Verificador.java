9761dd5c2fa41582d9b1b0118a6d5dd2230b836b
saluhera2023 <saluhera2023@gmail.com>
2026-03-16 20:22:34 -0500

    Update Verificador.java

--- Tema_01/Actividad_01/Verificador.java
+++ Tema_01/Actividad_01/Verificador.java
@@ -1,5 +1,9 @@
+package Actividad_01;
+
 public class Verificador {
-public static boolean esSobrePos(Rectangulo r1, Rectangulo r2) {
+
+    public static boolean esSobrePos(Rectangulo r1, Rectangulo r2) {
+
         double r1_minX = r1.getEsquina1().getX();
         double r1_minY = r1.getEsquina1().getY();
         double r1_maxX = r1.getEsquina2().getX();
@@ -9,29 +13,35 @@ public static boolean esSobrePos(Rectangulo r1, Rectangulo r2) {
         double r2_minY = r2.getEsquina1().getY();
         double r2_maxX = r2.getEsquina2().getX();
         double r2_maxY = r2.getEsquina2().getY();
-        
+
         boolean shrz = (r1_maxX < r2_minX) || (r1_minX > r2_maxX);
         boolean sver = (r1_maxY < r2_minY) || (r1_minY > r2_maxY);
+
         if (!shrz && !sver) {
             return true;
         } else {
             return false;
         }
     }
-        
-public static boolean esJunto(Rectangulo r1, Rectangulo r2) {
-        if (esSobrePos(r1, r2)) return false;       
+
+    public static boolean esJunto(Rectangulo r1, Rectangulo r2) {
+
+        if (esSobrePos(r1, r2)) return false;
+
         double r1_minX = r1.getEsquina1().getX();
         double r1_maxX = r1.getEsquina2().getX();
         double r2_minX = r2.getEsquina1().getX();
         double r2_maxX = r2.getEsquina2().getX();
-        
+
         boolean tcX = (r1_maxX == r2_minX) || (r1_minX == r2_maxX);
-        boolean tcY = (r1.getEsquina2().getY() == r2.getEsquina1().getY()) || (r1.getEsquina1().getY() == r2.getEsquina2().getY());
+        boolean tcY =
+            (r1.getEsquina2().getY() == r2.getEsquina1().getY()) ||
+            (r1.getEsquina1().getY() == r2.getEsquina2().getY());
+
         return tcX || tcY;
     }
 
-public static boolean esDisjunto(Rectangulo r1, Rectangulo r2) {
+    public static boolean esDisjunto(Rectangulo r1, Rectangulo r2) {
         return !esSobrePos(r1, r2) && !esJunto(r1, r2);
     }
 }
