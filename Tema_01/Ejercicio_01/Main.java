f5b2f2d6493ebaf4116edcead0e1655a37974f32
saluhera2023 <saluhera2023@gmail.com>
2026-03-17 07:08:45 -0500

    Update Main.java

--- Tema_01/Ejercicio_01/Main.java
+++ Tema_01/Ejercicio_01/Main.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Ingrese la cantidad maxima de rectangulos: ");
+        System.out.print("Ingrese la cantidad de rectangulos a almacenar: ");
         int n = sc.nextInt();
         ContainerRect contenedor = new ContainerRect(n);
 
         for (int i = 0; i < n; i++) {
-            System.out.println("\nRectangulo " + (i + 1));
-            System.out.print("Ingrese una esquina: ");
+            System.out.println("\nRectangulo " + (i + 1) + ":");
+            System.out.print("Ingrese una esquina del rectangulo: ");
             double x1 = sc.nextDouble();
             double y1 = sc.nextDouble();
             Coordenada c1 = new Coordenada(x1, y1);
 
-            System.out.print("Ingrese la esquina opuesta: ");
+            System.out.print("Ingrese la esquina opuesta del rectangulo: ");
             double x2 = sc.nextDouble();
             double y2 = sc.nextDouble();
             Coordenada c2 = new Coordenada(x2, y2);
@@ -24,7 +24,7 @@ public class Main {
             contenedor.addRectangulo(r);
         }
 
-        System.out.println("\nListado de rectangulos almacenados:");
+        System.out.println("\nRectangulos almacenados:");
         System.out.println(contenedor.toString());
 
         sc.close();
