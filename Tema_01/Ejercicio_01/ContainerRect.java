5483c184b91324bc163872cb527bbec87e1a6ed3
saluhera2023 <saluhera2023@gmail.com>
2026-03-16 23:32:48 -0500

    Create ContainerRect.java

---
+++ Tema_01/Ejercicio_01/ContainerRect.java
@@ -0,0 +1,34 @@
+public class ContainerRect {
+    private Rectangulo[] rectangulos;
+    private double[] distancias;
+    private double[] areas;
+    private int n;
+    private static int numRec = 0;
+
+    public ContainerRect(int n) {
+        this.n = n;
+        this.rectangulos = new Rectangulo[n];
+        this.distancias = new double[n];
+        this.areas = new double[n];
+    }
+
+    public void addRectangulo(Rectangulo r) {
+        if (numRec < n) {
+            rectangulos[numRec] = r;
+            distancias[numRec] = r.getEsquina1().distancia(r.getEsquina2());
+            areas[numRec] = r.calculoArea();
+            numRec++;
+        } else {
+            System.out.println("Ya no es posible guardar el rectangulo");
+        }
+    }
+
+    public String toString() {
+        String res = String.format("%-12s %-30s %-12s %-10s\n", "Rectangulo", "Coordenadas", "Distancia", "Area");
+        for (int i = 0; i < numRec; i++) {
+            res += String.format("%-12d %-30s %-12.3f %-10.2f\n", 
+                    (i + 1), rectangulos[i].toString(), distancias[i], areas[i]);
+        }
+        return res;
+    }
+}
