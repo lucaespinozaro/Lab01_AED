f1549903896f785eb65034118f8818a0212bfaa9
saluhera2023 <saluhera2023@gmail.com>
2026-03-17 06:41:52 -0500

    Update Rectangulo.java

--- Tema_01/Ejercicio_01/Rectangulo.java
+++ Tema_01/Ejercicio_01/Rectangulo.java
@@ -1,14 +1,28 @@
 import java.util.*;
 
 public class Rectangulo {
+
     private Coordenada esquina1;
     private Coordenada esquina2;
 
     public Rectangulo(Coordenada c1, Coordenada c2) {
-        double minX = Math.min(c1.getX(), c2.getX());
-        double minY = Math.min(c1.getY(), c2.getY());
-        double maxX = Math.max(c1.getX(), c2.getX());
-        double maxY = Math.max(c1.getY(), c2.getY());
+        double minX, minY, maxX, maxY;
+
+        if (c1.getX() < c2.getX()) {
+            minX = c1.getX();
+            maxX = c2.getX();
+        } else {
+            minX = c2.getX();
+            maxX = c1.getX();
+        }
+
+        if (c1.getY() < c2.getY()) {
+            minY = c1.getY();
+            maxY = c2.getY();
+        } else {
+            minY = c2.getY();
+            maxY = c1.getY();
+        }
         
         this.esquina1 = new Coordenada(minX, minY);
         this.esquina2 = new Coordenada(maxX, maxY);
