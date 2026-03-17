6b76aeba3f05e914e0358cab1e1c1aba3e00cce8
saluhera2023 <saluhera2023@gmail.com>
2026-03-17 07:48:55 -0500

    Update Main.java

--- Tema_01/Ejercicio_02/Main.java
+++ Tema_01/Ejercicio_02/Main.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la ruta del archivo de datos (.txt): ");
+        String ruta = sc.nextLine();
+
+        MatrizMinera matriz = LectorArchivo.leerArchivo(ruta);
+
+        if (matriz == null) {
+            System.out.println("No se pudo cargar la matriz. Terminando programa.");
+            sc.close();
+            return;
+        }
+
+        System.out.print("Ingrese el tamano k de la subregion cuadrada (k x k): ");
+        int k = sc.nextInt();
+
+        matriz.mostrarMejorSubregion(k);
+
+        sc.close();
+    }
+}
