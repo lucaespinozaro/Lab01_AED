da5636158fe0ae43074dda80c580e69e59616770
saluhera2023 <saluhera2023@gmail.com>
2026-03-17 07:50:06 -0500

    Create Zona.java

---
+++ Tema_01/Ejercicio_02/Zona.java
@@ -0,0 +1,33 @@
+public class Zona {
+    private String tipoMineral;
+    private int cantidad;
+    private double pureza;
+
+    public Zona(String tipoMineral, int cantidad, double pureza) {
+        this.tipoMineral = tipoMineral;
+        this.cantidad = cantidad;
+        this.pureza = pureza;
+    }
+
+    public double getValorEconomico() {
+        return cantidad * pureza;
+    }
+
+    public String getTipoMineral() {
+        return tipoMineral;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPureza() {
+        return pureza;
+    }
+
+    @Override
+    public String toString() {
+        return tipoMineral + " | Cantidad: " + cantidad + " | Pureza: " + pureza
+                + " | Valor: " + String.format("%.2f", getValorEconomico());
+    }
+}
